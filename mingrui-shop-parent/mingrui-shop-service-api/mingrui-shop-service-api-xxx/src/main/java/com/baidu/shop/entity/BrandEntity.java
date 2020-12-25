16775c62af2751d345cd67035177a1c90c794e9b
yx-long <yxl971122@163.com>
2020-12-25 14:03:35 +0800

    band查询

--- mingrui-shop-parent/mingrui-shop-service-api/mingrui-shop-service-api-xxx/src/main/java/com/baidu/shop/entity/BrandEntity.java
+++ mingrui-shop-parent/mingrui-shop-service-api/mingrui-shop-service-api-xxx/src/main/java/com/baidu/shop/entity/BrandEntity.java
@@ -1,4 +1,21 @@
 package com.baidu.shop.entity;
 
+import lombok.Data;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Table(name = "tb_brand")
 public class BrandEntity {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    private String image;
+
+    private String letter;
+
 }
